dda2a5258ee772db8b607b8437b7eb88c38652db
Samson Kitigo <samsonjesuyon@gmail.com>
2025-10-16 23:41:44 -0500

    updated env

--- ADS_Dental/ads_backend/app/core/config.py
+++ ADS_Dental/ads_backend/app/core/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql+aiomysql://root:password@localhost:3306/ADSDentalSurgeryDB")
+DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-key")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
