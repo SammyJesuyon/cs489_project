24ae6705222b5ec696e17c725e49ff71f480f34d
Samson Kitigo <samsonjesuyon@gmail.com>
2025-10-17 10:38:10 -0500

    surgery endpoint

--- ADS_Dental/ads_backend/app/api/endpoints/surgery.py
+++ ADS_Dental/ads_backend/app/api/endpoints/surgery.py
@@ -5,9 +5,10 @@ from app.api.dependencies.rbac import require_role
 from app.db.session import get_database
 from app.schemas.surgery_dto import SurgeryDTO
 from app.services.surgery_service import list_surgeries_service
+from typing import List
 
 router = APIRouter(prefix="/adsweb/api/v1", tags=["Surgeries"])
 
-@router.get("/surgeries", response_model=SurgeryDTO, dependencies=[Depends(require_role(["PATIENT", "ADMIN"]))])
+@router.get("/surgeries", response_model=List[SurgeryDTO], dependencies=[Depends(require_role(["PATIENT", "ADMIN"]))])
 async def list_surgeries(db : AsyncSession = Depends(get_database)):
     return await list_surgeries_service(db)
